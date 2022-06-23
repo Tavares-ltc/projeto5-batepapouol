62bd11d7ea850b08a3274098e2da6042a8d9f30a
Lucas Tavares <l.tavaresc@hotmail.com>
2022-06-23 18:39:51 -0300

    Agora o site verfica se o usuario continuar online

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let username, hora, mensagens
-
+let username, hora, userStatus
+let mensagens = []
 let msg = document.querySelector('.container-msg')
 
 msg = 'lorem impsum teste porcaria do teste caramba loucura loucura'
@@ -11,28 +11,22 @@ function userName() {
     const texto = document.querySelector('.bottom input').value
     const login = { name: username };
 
+
+
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", login);
-
-    const mensagem =
-    {
-        from: userName,
-        to: "Todos",
-        text: texto,
-        type: "message" // ou "private_message" para o bônus
-    };
-
-    let postagemMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
-
-    postagemMensagem.then()
-    postagemMensagem.catch()
-
     promise.then(seguir)
     promise.catch(tratarErro)
+}
+function verificaOnline() {
+    username = input.value
+    const login = { name: username }
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", login)
+    console.log(`${username} está online.`)
 }
 
 function tratarErro(erro) {
     erro = erro.response.status;
-    if (erro === 400){
+    if (erro === 400) {
         alert(`Erro ${erro}, nome ${input.value} já em uso...`)
         input.value = ''
         Location.reload
@@ -42,23 +36,34 @@ function tratarErro(erro) {
         Location.reload
     }
 }
-function logon_off() {
+function logOn_Off() {
     let entrada = document.querySelector('.entrada')
     let chat = document.querySelector('.page')
     entrada.classList.toggle('escondido')
     chat.classList.toggle('escondido')
 }
 function seguir(teste) {
-    logon_off();
-    let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-    promise.then(processarResposta)
+    userStatus = true
+    logOn_Off();
+    setInterval(verificaOnline, 5000)
+    setInterval(buscaMsg, 1000)
 
 
     //     <div class="msg">
     //         ${hora} <strong>${username}</strong> entrou na sala...
     //     </div>
 }
-function processarResposta(resposta) {
-    console.log(resposta.data)
-    mensagens = resposta.data
+function buscaMsg() {
+    let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    promise.then(historico)
+}
+function historico(lista) {
+    if (mensagens.includes(lista.data)){
+        return;
+    }
+    else {
+        mensagens.push(lista.data)
+    }
+
+    console.log(mensagens)
 }
