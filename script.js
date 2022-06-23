4767c6f8e5b2b02f94abe4b17dcb045306f3ae53
Lucas Tavares <l.tavaresc@hotmail.com>
2022-06-23 16:07:04 -0300

    Agora é possível logar no servidor

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-let username, hora
+let username, hora, mensagens
 
 let msg = document.querySelector('.container-msg')
 
 msg = 'lorem impsum teste porcaria do teste caramba loucura loucura'
 hora = '(09:21:45)'
+let input = document.querySelector('input')
 
 function userName() {
-    let input = document.querySelector('input')
     username = input.value
+    const texto = document.querySelector('.bottom input').value
+    const login = { name: username };
+
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", login);
+
+    const mensagem =
+    {
+        from: userName,
+        to: "Todos",
+        text: texto,
+        type: "message" // ou "private_message" para o bônus
+    };
+
+    let postagemMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
+
+    postagemMensagem.then()
+    postagemMensagem.catch()
+
+    promise.then(seguir)
+    promise.catch(tratarErro)
+}
+
+function tratarErro(erro) {
+    erro = erro.response.status;
+    if (erro === 400){
+        alert(`Erro ${erro}, nome ${input.value} já em uso...`)
+        input.value = ''
+        Location.reload
+    }
+    else {
+        alert(`Erro ${erro}, tente novamente mais tarde.`)
+        Location.reload
+    }
+}
+function logon_off() {
     let entrada = document.querySelector('.entrada')
-    entrada.classList.add('escondido')
     let chat = document.querySelector('.page')
-    chat.classList.remove('escondido')
-    msg.innerHTML +=
-        `
+    entrada.classList.toggle('escondido')
+    chat.classList.toggle('escondido')
+}
+function seguir(teste) {
+    logon_off();
+    let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    promise.then(processarResposta)
 
-        <div class="msg">
-            ${hora} <strong>${username}</strong> entrou na sala...
-        </div>
 
-    `
+    //     <div class="msg">
+    //         ${hora} <strong>${username}</strong> entrou na sala...
+    //     </div>
+}
+function processarResposta(resposta) {
+    console.log(resposta.data)
+    mensagens = resposta.data
 }
